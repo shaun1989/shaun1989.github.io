32abaef0b80207a74f600c5724b52a1ca9ab5c72
shaun <hughes.shaun1989@gmail.com>
2018-11-27 22:03:03 +0000

    update

--- site.js
+++ site.js
@@ -1,1 +1,3 @@
-window.onload = alert("hello and welcome to my page at the moment it is a work in progress so bare with me");
+window.onload = function welcome() {
+	alert("hello and welcome to my page at the moment it is a work in progress so bare with me");
+}
