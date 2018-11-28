094a162f8490738f430aeb44f87da534ced1cb2a
shaun <hughes.shaun1989@gmail.com>
2018-11-28 01:39:24 +0000

    updated interface on search function

--- site.js
+++ site.js
@@ -2,37 +2,33 @@ window.onload = function welcome() {
 	alert("hello and welcome to my page at the moment it is a work in progress so bare/n with me this version is currently only for laptops mobile to come soon");
 }
 const learn = [{
-	"id": "standard", 
-	    "name":"cmd",
-		"move to folder": "cd Documents/foldername/foldername",
-		"create folder": "mkdir filename",
-		"rename": "mv filename , newfilename",
-		"delete": "rmdir filename",
-		"view file": "cat ~/filename",
+	"id": "standard cmd", 
+		"move to folder": "cd Documents/foldername/foldername <br />",
+		"create folder": "mkdir filename <br /> ",
+		"rename": "mv filename , newfilename <br />",
+		"delete": "rmdir filename <br />",
+		"view file": "cat ~/filename<br /> ",
 		"move a folder": "move filename destinationFolder"
 },
 {
-    "id": "rvm",
-        "name": "cmd",
-        "run": "source home/name/.rvm/scripts/",
-        "use": "use 2.5.3",
-        "default": "use --default 2.5.3",
-        "config": "rvm config -l or --list"
+    "id": "rvm cmd",
+        "run": "source home/name/.rvm/scripts/ <br />",
+        "use": "use 2.5.3 <br />",
+        "default": "use --default 2.5.3 <br />",
+        "config": "rvm config -l or --list <br />"
 },
 {
-    "id": "rvm1",
-    "name": "install",
-    "required": "curl , most recent os update , git recommended",
-    "command": "curl -l get.rvm.oi | bash -s stable"
+    "id": "rvm install",
+    "required": "curl , most recent os update , git recommended <br />",
+    "command": "curl -l get.rvm.oi | bash -s stable <br />"
 
 }];
-function search (id , name , prop) {
+function search (id  ,prop) {
     for (x = 0; x < learn.length; x++) {
-        if (learn[x].id === id ) {
-            if (name === name && prop === "") {
+        if (learn[x].id === id) {
+            if ( prop === "") {
             return JSON.stringify(learn[x]);
          } else if (learn[x].id === id ) {
-            if (learn[x].name === name) {
                 if (learn[x].hasOwnProperty(prop)) {
                     return learn[x][prop];
                 } else {
@@ -41,7 +37,6 @@ function search (id , name , prop) {
             }
         }
 }
-}
 return "please enter a valid search";
 }
-document.write(search("rvm1","install","required"));
+document.write(search("rvm cmd","run",""));
